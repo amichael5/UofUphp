e767e6651de71f9118df77c182c4f5212e5fe081
amichael5 <36896672+amichael5@users.noreply.github.com>
2020-03-28 11:43:13 -0600

    Create magazine_update.php

---
+++ magazine/magazine_update.php
@@ -0,0 +1,40 @@
+<?php
+	echo <<<_END
+		<html>
+			<head>
+				<title>Update Magazine</title>
+				<link rel='stylesheet' href='../styles.css'>
+			</head>
+			<body>
+				<a>
+					<br>
+					<center>
+					<img height='100' width='200' src='../images/library_logo.jpg'></img>
+					</center>
+					<br>
+					<br>
+				</a>
+				<form method="post" action ="magazine_inventory.php">
+					<center>
+					Magazine ID:
+					<input type='text' name='id'>
+					<br>
+					Magazine Name:
+					<input type='text' name='name'>
+					<br>
+					Topic:
+					<input type='text' name='type'>
+					<br>
+					Publisher:
+					<input type='text' name='value'>
+					<br>
+					Publish Year:
+					<input type='text' name='points'>
+					<br>
+					<input type='submit' value='update'>
+					</center>
+				</form>
+			</body>
+		</html>
+	_END;
+?>
