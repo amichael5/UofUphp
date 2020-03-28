094bb51180b008643d804726500ba09a4c6cfb2b
amichael5 <36896672+amichael5@users.noreply.github.com>
2020-03-28 11:46:24 -0600

    Create magazine_delete.php

---
+++ magazine/magazine_delete.php
@@ -0,0 +1,20 @@
+<?php
+	echo <<<_END
+		<html>
+			<head>
+				<title>Magazine Delete</title>
+				<link rel='stylesheet' href='../styles.css'>
+			</head>
+			<body>
+				<a>
+					<br>
+					<center>
+					<img height='100' width='200' src='../images/library_logo.jpg'></img>
+					</center>
+					<br>
+					<br>
+				</a>
+			</body>
+		</html>
+	_END;
+?>
